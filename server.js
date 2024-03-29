8d9fcf7d24a57e759d5c74e8e1709cb85ee0bdc2
Paulo-cds <paulo.pdomingues@hotmail.com>
2024-03-28 21:29:49 -0300

    fix

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require("express")
 const http = require("http")
 const app = express()
 const server = http.createServer(app)
+// const io = require("socket.io")(server, {
+// 	allowRequest: (req, callback) => {
+// 	  const noOriginHeader = req.headers.origin === undefined;
+// 	  callback(null, noOriginHeader);
+// 	}
+//   });
 const io = require("socket.io")(server, {
 	cors: {
 		origin: "http://localhost:3000",
